148f67b1a6f0bca3de6760b9531223c35cbdb934
Madsen90 <frederikmusen@gmail.com>
2015-10-18 15:58:30 +0200

    mangler kun tests

---
+++ Mand2/Opgave 2/2.3/concurrentList.h
@@ -0,0 +1,30 @@
+/******************************************************************************
+   list.h 
+
+   Header file with definition of a simple linked list.
+
+******************************************************************************/
+
+#ifndef _CONCURRENTLIST_H
+#define _CONCURRENTLIST_H
+
+/* structures */
+typedef struct node {
+  void *elm; /* use void type for generality; we cast the element's type to void type */
+  struct node *next;
+} Node;
+
+typedef struct concurrentList {
+  int len;
+  Node *first;
+  Node *last;
+} ConcurrentList;
+
+/* functions */
+ConcurrentList *list_new(void);            /* return a new list structure */
+void list_add(ConcurrentList *l, Node *n); /* add node n to list l as the last element */
+Node *list_remove(ConcurrentList *l);      /* remove and return the first element from list l*/
+Node *node_new(void);            /* return a new node structure */
+Node *node_new_str(char *s);     /* return a new node structure, where elm points to new copy of string s */
+
+#endif
