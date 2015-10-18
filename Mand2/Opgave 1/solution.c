c55b3b189ba487bc13c5442ab77746ab2fbfe1a8
blaavogn <blaa.vogn@gmail.com>
2015-10-18 01:01:19 +0100

    Opgave 1 og 2

---
+++ Mand2/Opgave 1/solution.c
@@ -0,0 +1,83 @@
+#include <pthread.h>
+#include <math.h>
+#include <stdio.h>
+
+typedef struct td_ThreadData {
+	int lower;
+	int upper;
+	double partialSum;
+} ThreadData;
+
+void *runner(void *param); //threads call this function
+
+int main(int argc, char const *argv[])
+{
+	double sum = 0; //this data is shared by the thread(s)
+
+	if (argc != 3) {
+		fprintf(stderr, "usage: a.out <integer value>\n");
+		return -1;
+	}
+	if(atoi(argv[1]) < 0){
+		fprintf(stderr, "%d must be >= 0\n", atoi(argv[1]));
+	}
+	if(atoi(argv[2]) < 0){
+		fprintf(stderr, "%d must be >= 0\n", atoi(argv[2]));
+	}
+
+	int threads 	= atoi(argv[1]); 
+	int upperLimit  = atoi(argv[2]);
+	int sumSegment  = upperLimit / threads; 
+
+	ThreadData threadData[threads]; 
+	pthread_t tid[threads]; //Thread identifier
+	//pthread_attr_t attr[threads]; //set of thread attributes
+
+	
+		int i;
+		for(i = 0; i < threads; i++){
+			threadData[i].lower 	 = i * sumSegment + 1;
+			threadData[i].partialSum = 0;
+			
+			if(i+1 == threads)
+				threadData[i].upper = upperLimit;
+			else
+				threadData[i].upper = i * sumSegment + sumSegment;
+				
+			
+			//get the default attributes
+			//pthread_attr_init(&attr[i]);
+			//create the thread
+			pthread_create(&tid[i], NULL, runner, &threadData[i]);
+		}
+	
+
+	printf("---- \n");
+	
+		i = 0;
+		for(i = 0; i < threads; i++){
+			pthread_join(tid[i], NULL);
+			sum += threadData[i].partialSum;
+		}
+
+	printf("sum = %G\n", sum);
+
+	return 0;
+}
+
+//The thread will begin control in this function
+void *runner(void *param){
+	printf("Starting thread \n");
+
+	ThreadData* data = param;
+	
+	double sum = 0;
+
+	int i;
+	for(i = data->lower; i <= data->upper; i++){
+		sum += sqrt(i);
+	}
+	data->partialSum = sum;
+	printf("done\n");
+	pthread_exit(NULL);
+}
---
+++ Mand2/Opgave 2/Holger/list.c
@@ -0,0 +1,81 @@
+/******************************************************************************
+   list.c
+
+   Implementation of simple linked list defined in list.h.
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include "list.h"
+
+/* list_new: return a new list structure */
+List *list_new(void)
+{
+  List *l;
+
+  l = (List *) malloc(sizeof(List));
+  l->len = 0;
+  pthread_mutex_init(&l->mutex, NULL);
+
+  /* insert root element which should never be removed */
+  l->first = l->last = (Node *) malloc(sizeof(Node));
+  l->first->elm = NULL;
+  l->first->next = NULL;
+  return l;
+}
+
+/* list_add: add node n to list l as the last element */
+//Might make the end element to a circle by adding the same element twice
+void list_add(List *l, Node *n)
+{
+  pthread_mutex_lock(&l->mutex);
+
+  Node* it = l->first;
+  while(it->next != NULL){
+    it = it->next;
+  }
+
+  it->next = n;
+  l->last = n;
+  l->len++;
+  pthread_mutex_unlock(&l->mutex);
+}
+
+/* list_remove: remove and return the first (non-root) element from list l */
+Node *list_remove(List *l)
+{
+  pthread_mutex_lock(&l->mutex);
+  Node *n = l->first->next;
+  
+  if(n != NULL){
+    l->first->next = n->next;
+    l->len--;
+  }
+  pthread_mutex_unlock(&l->mutex);
+  
+  return n;
+}
+
+/* node_new: return a new node structure */
+Node *node_new(void)
+{
+  Node *n;
+  n = (Node *) malloc(sizeof(Node));
+  n->elm = NULL;
+  n->next = NULL;
+  return n;
+}
+
+/* node_new_str: return a new node structure, where elm points to new copy of s */
+Node *node_new_str(char *s)
+{
+  Node *n;
+  n = (Node *) malloc(sizeof(Node));
+  n->elm = (void *) malloc((strlen(s)+1) * sizeof(char));
+  strcpy((char *) n->elm, s);
+  n->next = NULL;
+  return n;
+}
