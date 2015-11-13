7baf7a866e0f29dcb54eec16d8da31c034878703
Madsen90 <frederikmusen@gmail.com>
2015-11-13 13:33:26 +0100

    added test env

---
+++ Mand3/OO3/test.c
@@ -0,0 +1,21 @@
+#include "minunit.h"
+
+
+int tests_run = 0;
+
+static char * test_add() {}
+
+static char * all_tests() {}
+
+int main(int argc, char **argv) {
+	char *result = all_tests();
+	 if (result != 0) {
+	     printf("%s\n", result);
+	 }
+	 else {
+	     printf("ALL TESTS PASSED\n");
+	 }
+	 printf("Tests run: %d\n", tests_run);
+
+	 return result != 0;
+}
