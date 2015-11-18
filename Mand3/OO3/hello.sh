acbc83e76657f8494e0c84b93f9ddc78d033655f
Madsen90 <frederikmusen@gmail.com>
2015-11-18 12:31:32 +0100

    new output

--- Mand3/OO3/hello.sh
+++ Mand3/OO3/hello.sh
@@ -9,6 +9,10 @@ do
 	   ./virtmem 100 $b fifo sort
 	   ./virtmem 100 $b fifo focus
 	   ./virtmem 100 $b fifo scan
+	   echo "RAND" 
+	   ./virtmem 100 $b rand sort
+	   ./virtmem 100 $b rand focus
+	   ./virtmem 100 $b rand scan
 	   echo "CUSTOM" 
 	   ./virtmem 100 $b custom sort
 	   ./virtmem 100 $b custom focus
